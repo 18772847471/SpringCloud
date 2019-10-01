2dab9ab813f351803713c116915e60b46ffd749b
黄博 <1104583639@qq.com>
2019-10-02 01:09:27 +0800

    springcloud-eureka代码

---
+++ springcloud-eureka-server/src/main/java/com/example/springcloudeurekaserver/SpringcloudEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloudeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudEurekaServerApplication.class, args);
+    }
+
+}
