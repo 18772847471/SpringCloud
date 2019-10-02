b341bc003061b64a296577c4469cd117646279d5
黄博 <1104583639@qq.com>
2019-10-02 20:53:11 +0800

    springcloud-eureka代码

---
+++ sprincloud-common/src/main/java/com/example/SpringCloudCommonApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudCommonApplication.class, args);
+    }
+
+}
