5bda0be1b536a88fc4f03706363cae1a0f14c251
黄博 <1104583639@qq.com>
2019-10-07 23:38:38 +0800

    Hystrix服务监控Dashboard

--- springcloud-consumer-hystrix-dashboard/src/main/java/com/example/SpringCloudConsumerHystrixDashboardApplication.java
+++ springcloud-consumer-hystrix-dashboard/src/main/java/com/example/SpringCloudConsumerHystrixDashboardApplication.java
@@ -3,11 +3,15 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
-@EnableHystrixDashboard
+@EnableEurekaClient
+@EnableFeignClients
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class SpringCloudConsumerHystrixDashboardApplication {
 
     public static void main(String[] args) {
---
+++ springcloud-consumer-hystrix-dashboard/src/main/java/com/example/business/impl/PeopleServiceImpl.java
@@ -0,0 +1,22 @@
+package com.example.business.impl;
+
+import com.example.business.service.ClientService;
+import com.example.business.service.PeopleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("peopleServiceImpl")
+public class PeopleServiceImpl implements PeopleService {
+    @Autowired
+    private ClientService clientService;
+
+    @Override
+    public String hello() {
+        return clientService.hello();
+    }
+
+    @Override
+    public String getinfo() {
+        return clientService.getinfo();
+    }
+}
